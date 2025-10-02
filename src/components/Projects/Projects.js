324a0e7fc1b7cd0a8eb58f7623309944d0a7473e
hana437 <hanahabtemicheal@gmail.com>
2025-10-02 11:25:50 +0300

    update links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,6 @@ function Projects() {
               isBlog={false}
               title="Dc Dashboard"
               description="DC Portal is a web-based data collection and project management platform designed to streamline how project manage farmer information and field operations. The system empowers users to efficiently collect, update, and analyze farmer data while also managing company-led agricultural projects through customizable, dynamic forms."
-              ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="http://51.20.140.67:8097"
             />
           </Col>
@@ -38,7 +37,6 @@ function Projects() {
               isBlog={false}
               title="Credit Score Dashboard"
               description="As part of Lersha’s partnership with financial institutions, I developed a Credit Score and Loan Management Dashboard that enables lersha's and banks to provide and track agricultural loans for farmers. The system is built with multi-bank support, allowing each partner bank to operate with separate branding, logic, and workflows, all within one unified platform."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
               demoLink="http://51.20.140.67:8094"
             />
           </Col>
@@ -49,8 +47,7 @@ function Projects() {
               isBlog={false}
               title="Lersha Dashboard"
               description="The Lersha Admin Portal is a robust administrative dashboard designed to give full control over the Lersha ecosystem. It allows admins to manage users, monitor transactions, oversee product listings, and track mechanization service orders in real-time."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              demoLink="http://51.20.140.67:8060"              
             />
           </Col>
 
@@ -60,8 +57,7 @@ function Projects() {
               isBlog={false}
               title="Contrat Farming"
               description="As part of my work in the agricultural technology sector, I developed an integrated contract farming platform that simplifies key processes for farmers and processors. This includes a streamlined farmer registration system for both individual and lead farmers, an intuitive interface for capturing and managing detailed farmer visit reports, and an efficient tool for handling contract agreements. The platform enhances transparency, improves collaboration, and optimizes the management of farming operations, fostering stronger partnerships and boosting productivity in the agricultural industry."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              demoLink="http://164.160.187.141:8099"
             />
           </Col>
 
@@ -71,8 +67,7 @@ function Projects() {
               isBlog={false}
               title="Custmer base system"
               description="I also developed a customer base   dashboard to retrieve up-to-date service and project information, manage FAQs, and assign follow-up tasks to relevant internal teams. The system bridges field operations, customer support, and internal collaboration — ensuring consistent, efficient communication across all user types"
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              demoLink="164.160.187.141:3025"
             />
           </Col>
 
